0b0bf2e6e1c3df00ac5673b65ba41d4ec6ddbaf5
macorisd <macorisd@github.com>
2026-01-27 13:39:52 +0100

    Update QoS settings for get_class_distributions service (for compatibility across different ROS 2 versions)

--- voxeland/voxeland/src/voxeland_server.cpp
+++ voxeland/voxeland/src/voxeland_server.cpp
@@ -274,7 +274,7 @@ namespace voxeland_server
 
             get_distributions_srv_ = create_service<GetClassDistributions>("voxeland/get_class_distributions",
                                                                            std::bind(&VoxelandServer::getClassDistributionsSrv, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
-                                                                           rclcpp::QoS(1).reliable());
+                                                                           rmw_qos_profile_services_default);
         }
 
         if (bonxai_.get() == nullptr)
