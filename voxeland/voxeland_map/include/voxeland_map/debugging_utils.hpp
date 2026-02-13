230c77e5bb72862c06b6383ef16dbb99c73e70d6
PepeOjeda <ojedamorala@hotmail.com>
2026-02-13 13:41:43 +0100

    added sleeping to debug pause

--- voxeland/voxeland_map/include/voxeland_map/debugging_utils.hpp
+++ voxeland/voxeland_map/include/voxeland_map/debugging_utils.hpp
@@ -13,16 +13,16 @@ namespace debugging_utils
 }  // namespace debugging_utils
 
 #if ENABLE_DEBUG_GUI
-#define PAUSE_THREAD_UNTIL_GUI_CONTINUE(condition)     \
-    if (condition)                                     \
-    {                                                  \
-        fprintf(stderr, "THREAD PAUSED\n");            \
-        debugging_utils::debug_paused = true;          \
-        debugging_utils::mutex.unlock();               \
-        while (debugging_utils::debug_paused)          \
-            ;                                          \
-        debugging_utils::mutex.lock();                 \
-        fprintf(stderr, "THREAD EXECUTION RESUMED\n"); \
+#define PAUSE_THREAD_UNTIL_GUI_CONTINUE(condition)                      \
+    if (condition)                                                      \
+    {                                                                   \
+        fprintf(stderr, "THREAD PAUSED\n");                             \
+        debugging_utils::debug_paused = true;                           \
+        debugging_utils::mutex.unlock();                                \
+        while (debugging_utils::debug_paused)                           \
+            std::this_thread::sleep_for(std::chrono::milliseconds(15)); \
+        debugging_utils::mutex.lock();                                  \
+        fprintf(stderr, "THREAD EXECUTION RESUMED\n");                  \
     }
 #else
 #define PAUSE_THREAD_UNTIL_GUI_CONTINUE(condition)
